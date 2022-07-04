10555656da9608333e499e7122a6bbba1efd00ee
Bekir - CDI <b.burakcebecii@gmail.com>
2022-07-04 19:29:01 +0200

    basic api with node..  initial commit..

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+const data = fs.readFileSync(`${__dirname}/dev-data/employees.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const server = http.createServer((req, res) => {
+  const pathName = req.url;
+  if (pathName === "/overview") {
+    res.end("This is the OVERVIEW");
+  } else if (pathName === "/product") {
+    res.end("This is the PRODUCT");
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    res.end(data);
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello-world",
+    });
+    res.end("<h1>Page not found!</h1>");
+  }
+});
+server.listen(8000, () => {
+  console.log("Listenning to requests on port 8000");
+});
